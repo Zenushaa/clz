5891a16bcb20e8ebaf4feae32bd0424f64dd1d24
[nirdesh shakya] <[nirkajishakya23@gmail.com]>
2024-11-26 08:05:40 +0545

    first commit

---
+++ test01/students.cs
@@ -0,0 +1,16 @@
+﻿namespace test01
+{
+    public class student
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string enrollmentDate { get; set; }
+        public string Address { get; set; }
+        public string CreatedDate { get; set; }
+        public string CreatedBy { get; set; }
+
+    }
+}
