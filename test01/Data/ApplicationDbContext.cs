dad9f6c4a96d067527231df7c61eac040b5b5d9a
[nirdesh shakya] <[nirkajishakya23@gmail.com]>
2024-12-10 08:12:41 +0545

    first commit

---
+++ test01/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace test01.Data;
+
+public class ApplicationDbContext :DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+
+    }
+    public DbSet<student> students { get; set; }
+}
--- test01/obj/Debug/net8.0/test01.AssemblyInfo.cs
+++ test01/obj/Debug/net8.0/test01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5891a16bcb20e8ebaf4feae32bd0424f64dd1d24")]
 [assembly: System.Reflection.AssemblyProductAttribute("test01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
